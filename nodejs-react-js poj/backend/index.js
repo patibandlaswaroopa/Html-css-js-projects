dc08a5ddf4021222de66618166fa2ee22cdb8f2e
patibandla swaroopa <127756900+patibandlaswaroopa@users.noreply.github.com>
2024-10-05 13:03:20 +0530

    Add files via upload

---
+++ nodejs-react-js poj/backend/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try{
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username:username, secret:username,first_name: username},
+        {headers:{"private-key": "bc0a7846-201c-4073-9d99-10460c9d2831"}}
+
+    )
+
+    return res.status(r.status).json(r.data);
+ }
+ catch(e){
+    return res.status(e.response.status).json(e.response.data);
+
+ }
+ 
+ // return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001);
